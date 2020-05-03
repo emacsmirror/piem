0c1da9c289854b040ceed22837b2002e4bbff2cf
Kyle Meyer <kyle@kyleam.com>
2020-05-03 11:16:56 -0400

    A pathetic piem

---
+++ piem.el
@@ -0,0 +1,7 @@
+;;; piem.el --- Emacs tools for working with public-index
+
+(defun piem-please ()
+  "How I wish I could intersect my emails, feeds, and repos")
+
+;;; piem.el ends here
+(provide 'piem)
